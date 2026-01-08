37d63d6a401d56e6f1d1971245f215ce8711f8cc
alvin081105 <ca081105@gmail.com>
2026-01-08 09:33:23 +0900

    채팅 상태변경 기능 추가

--- src/main/java/com/example/bumil_backend/controller/ChatController.java
+++ src/main/java/com/example/bumil_backend/controller/ChatController.java
@@ -1,17 +1,14 @@
 package com.example.bumil_backend.controller;
 
 import com.example.bumil_backend.common.ApiResponse;
+import com.example.bumil_backend.dto.chat.request.ChatCloseRequest;
 import com.example.bumil_backend.dto.chat.request.ChatCreateRequest;
 import com.example.bumil_backend.dto.chat.response.ChatCreateResponse;
-import com.example.bumil_backend.dto.chat.response.PublicChatListResponse;
 import com.example.bumil_backend.service.ChatService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/chats")
@@ -20,18 +17,23 @@ public class ChatController {
 
     private final ChatService chatService;
 
+
     @PostMapping
     public ResponseEntity<ApiResponse<ChatCreateResponse>> createChat(
             @RequestBody @Valid ChatCreateRequest request
     ) {
-        return ApiResponse.ok(chatService.createChat(request), "채팅방이 생성되었습니다.");
+        return ApiResponse.ok(
+                chatService.createChat(request),
+                "채팅방이 생성되었습니다."
+        );
     }
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<List<PublicChatListResponse>>> getPublicChatListResponse(
-            @RequestParam(value = "datefilter", required = false) String dateFilter,
-            @RequestParam(value = "tag", required = false) String tag
-    ){
-        return ApiResponse.ok(chatService.getPublicChatList(dateFilter, tag), "공개 채팅 목록 조회에 성공하였습니다.");
+
+    @PatchMapping("/close")
+    public ResponseEntity<ApiResponse<Void>> closeChat(
+            @RequestBody ChatCloseRequest request
+    ) {
+        chatService.closeChat(request);
+        return ApiResponse.ok(null, "채팅 상태가 변경되었습니다.");
     }
 }
